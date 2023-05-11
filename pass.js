244523487f0dd07e0a354464e243fcac0c7e183e
Ayush Kumar <ayushishan08@gmail.com>
2023-05-11 22:05:59 +0530

    first commit

---
+++ pass.js
@@ -0,0 +1,8 @@
+function validateForm() {
+    var username = document.forms["loginForm"]["username"].value;
+    var password = document.forms["loginForm"]["password"].value;
+    if (username == "" || password == "") {
+        alert("Username and password must be filled out");
+        return false;
+    }
+}
